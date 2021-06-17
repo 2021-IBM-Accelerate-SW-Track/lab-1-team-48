3a651ce4e38b38d0a483d70812377dfaf6c51b79
Raymond Castillo <60750068+raycastillo3@users.noreply.github.com>
2021-06-17 13:14:49 -0400

    Create Example.js

---
+++ src/component/Example.js
@@ -0,0 +1,6 @@
+//this component is for reference- you can use this as an example
+import React from 'react';
+
+//This is a JS arrow function 
+
+export const Example = () => <h1>Hello all, this component is just an example</h1>
